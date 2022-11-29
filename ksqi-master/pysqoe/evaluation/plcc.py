2cc7d7b1bd3cec62efb6928ee7ccb7a2de889409
Justin Guo <justin.guo01@gmail.com>
2022-11-28 20:13:57 -0500

    adding ksqi

---
+++ ksqi-master/pysqoe/evaluation/plcc.py
@@ -0,0 +1,14 @@
+from pysqoe.evaluation import Criterion
+from scipy.stats import pearsonr
+
+
+class PLCC(Criterion):
+    def __init__(self, version='default'):
+        super().__init__(version=version)
+
+    def __call__(self, obj_score, sbj_score):
+        obj_score = self._nonlinear_mapping(obj_score, sbj_score)
+        # print(obj_score)
+        sbj_score = [float(x) for x in sbj_score]
+        corr, _ = pearsonr(obj_score, sbj_score)
+        return corr
